044b2c6169bcdcab9b59bb94619ba15449db026e
Guzzini <pmguzzini@gmail.com>
2017-08-25 19:46:35 +0200

    Create User Screens

---
+++ bmf-app/src/app/create-user/capture-company.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'company',
+  templateUrl: './capture-company.component.html',
+  styleUrls: ['./capture-company.component.css']
+})
+
+export class CompanyRegistrationComponent implements OnInit{
+  constructor(){}
+  ngOnInit(){}
+}
