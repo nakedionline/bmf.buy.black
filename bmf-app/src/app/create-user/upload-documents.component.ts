655b718343cbd5a378dc626de680fd17d4d0667a
Guzzini <pmguzzini@gmail.com>
2017-08-25 19:49:47 +0200

    Create User Screens

---
+++ bmf-app/src/app/create-user/upload-documents.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'upload',
+  templateUrl: './upload-documents.component.html',
+  styleUrls: ['./upload-documents.component.css']
+})
+
+export class UploadDocumentsComponent implements OnInit{
+  constructor(){}
+  ngOnInit(){}
+}
