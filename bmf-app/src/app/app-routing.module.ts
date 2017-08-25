f26f66f90a1e3f4fc0e22b31782aec91a05e7446
Guzzini <pmguzzini@gmail.com>
2017-08-25 19:51:00 +0200

    Create User Screens

--- bmf-app/src/app/app-routing.module.ts
+++ bmf-app/src/app/app-routing.module.ts
@@ -6,16 +6,24 @@ import {DashboardComponent}     from './dash-board/dashboard.component';
 import {LandingComponent}       from './landing.component';
 import {AboutUsComponent}       from './about-us.component';
 import {ContactUsComponent} from "./contact-us.component";
+import {UserRegistrationComponent} from './create-user/user-registration.component';
+import {ShareholdersRegistrationComponent} from './create-user/capture-shareholders.component';
+import {CompanyRegistrationComponent} from './create-user/capture-company.component';
+
+
 
 const routes: Routes = [
   //{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
-  { path: 'landing',    component: LandingComponent },
-  { path: 'dashboard',  component: DashboardComponent },
-  { path: 'detail/:id', component: UserDetailComponent },
-  { path: 'users',      component: UsersComponent },
-  { path: 'about',      component: AboutUsComponent },
-  {path: 'contact',     component: ContactUsComponent}
+  { path: 'landing',      component: LandingComponent },
+  { path: 'dashboard',    component: DashboardComponent },
+  { path: 'detail/:id',   component: UserDetailComponent },
+  { path: 'users',        component: UsersComponent },
+  { path: 'about',        component: AboutUsComponent },
+  {path: 'contact',       component: ContactUsComponent},
+  {path: 'register',      component: UserRegistrationComponent},
+  {path: 'shareholders',  component: ShareholdersRegistrationComponent},
+  {path: 'company',       component: CompanyRegistrationComponent}
 ];
 
 @NgModule({
