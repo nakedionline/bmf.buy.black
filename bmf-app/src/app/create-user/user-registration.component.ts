b6def9fa614771913df8f8cdb2f5cd4f563278b5
Guzzini <pmguzzini@gmail.com>
2017-09-06 18:34:05 +0200

    Registration Redirects upload_html

--- bmf-app/src/app/create-user/user-registration.component.ts
+++ bmf-app/src/app/create-user/user-registration.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'register',
@@ -7,6 +8,10 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class UserRegistrationComponent implements OnInit{
-  constructor(){}
+  constructor(private router:Router){}
   ngOnInit(){}
+
+  gotoRegister(): void {
+    this.router.navigate(['/shareholders']);
+  }
 }
