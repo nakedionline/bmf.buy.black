b6a8cb5e3c44bc377960666aa9d8579f63f69169
Guzzini <pmguzzini@gmail.com>
2017-09-06 18:30:05 +0200

    Registration Redirects capture_company_component

--- bmf-app/src/app/create-user/capture-company.component.ts
+++ bmf-app/src/app/create-user/capture-company.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'company',
@@ -7,6 +8,10 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class CompanyRegistrationComponent implements OnInit{
-  constructor(){}
+  constructor(private router: Router){}
   ngOnInit(){}
+
+  gotoUpload(): void {
+    this.router.navigate(['/upload']);
+  }
 }
