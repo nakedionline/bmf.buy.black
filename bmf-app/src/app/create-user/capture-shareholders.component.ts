a190550f4dbfbe35b15b651815c0026aeb2e780c
Guzzini <pmguzzini@gmail.com>
2017-09-06 18:31:26 +0200

    Registration Redirects capture_shareholder_ts

--- bmf-app/src/app/create-user/capture-shareholders.component.ts
+++ bmf-app/src/app/create-user/capture-shareholders.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'shareholders',
@@ -7,6 +8,11 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class ShareholdersRegistrationComponent implements OnInit{
-  constructor(){}
+
+  constructor(private router: Router){}
   ngOnInit(){}
+
+  gotoCompany(): void {
+    this.router.navigate(['/company']);
+  }
 }
