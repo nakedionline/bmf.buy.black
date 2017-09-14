d04752075d1218ae3edc47be97e47cf8b08d1a9b
Guzzini <pmguzzini@gmail.com>
2017-09-14 18:11:45 +0200

    Shareholder_Screen_Validation

--- bmf-app/src/app/services/mock-users.ts
+++ bmf-app/src/app/services/mock-users.ts
@@ -1,12 +1,13 @@
 import {User} from './user';
 
 export const USERS: User[] = [
-  {id: 11, name: 'Joe Plumbers'},
+  /*{id: 11, name: 'Joe Plumbers'},
   {id: 12, name: 'Mkhambeni Caterer'},
   {id: 13, name: 'Panels in a Jeffie'},
   {id: 14, name: 'Mitsupi Enterprises'},
   {id: 15, name: 'Top Cake and Bakery'},
   {id: 16, name: 'Chuck Taylor Roofing'},
-  {id: 17, name: 'Zakheni Environmental Services'}
+  {id: 17, name: 'Zakheni Environmental Services'}*/
+  
 
 ];
