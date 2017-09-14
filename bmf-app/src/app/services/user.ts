80c3770f0e993bba611077f14ef2c8f9ff39857d
Guzzini <pmguzzini@gmail.com>
2017-09-14 18:12:07 +0200

    Shareholder_Screen_Validation

--- bmf-app/src/app/services/user.ts
+++ bmf-app/src/app/services/user.ts
@@ -1,4 +1,20 @@
 export class User{
-  id: number;
-  name: string;
+  id:       number;
+  title:    string;
+  name:     string;
+  surname:  string;
+  position: string;
+  idnumber: number;
+  gender:   string;
+
+  constructor(
+    public userid:        number,
+    public usertitle:     string,
+    public username:      string,
+    public usersurname:   string,
+    public userposition:  string,
+    public useridnumber:   number,
+    public usergender:    string,
+    public userservice:    string
+  ){}
 }
