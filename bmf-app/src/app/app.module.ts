71f7f1c33d334b5123ba8993ffa53a84f1793f82
Guzzini <pmguzzini@gmail.com>
2017-09-14 18:10:34 +0200

    Shareholder_Screen_Validation

--- bmf-app/src/app/app.module.ts
+++ bmf-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {UserDetailComponent} from './user-details/user-detail.component';
 import {UsersComponent} from './user-details/users.component';
@@ -36,6 +36,7 @@ import  {UploadDocumentsComponent} from './create-user/upload-documents.componen
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
     HttpModule
