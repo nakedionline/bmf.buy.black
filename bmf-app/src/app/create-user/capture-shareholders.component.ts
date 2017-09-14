525a5aa26e20f72e4c9b3b5cc29b18cb78058943
Guzzini <pmguzzini@gmail.com>
2017-09-14 18:11:24 +0200

    Shareholder_Screen_Validation

--- bmf-app/src/app/create-user/capture-shareholders.component.ts
+++ bmf-app/src/app/create-user/capture-shareholders.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {User} from '../services/user'
 
 @Component({
   selector: 'shareholders',
@@ -7,10 +8,50 @@ import {Router} from '@angular/router';
   styleUrls: ['./capture-shareholders.component.css']
 })
 
-export class ShareholdersRegistrationComponent implements OnInit{
+export class ShareholdersRegistrationComponent {
 
-  constructor(private router: Router){}
-  ngOnInit(){}
+
+  name: string;
+  services: Array<string>;
+  model = new User(18,'Dr','Peter','Lord','Head',78,'Male','Service');
+
+  submitted = false;
+
+  constructor(private router:Router){
+    this.services =  ['Plumbing', 'Electrical', 'Tiling', 'Decking'];
+  }
+
+  callName(){
+    this.name = 'Hi There';
+  }
+
+  onSubmit(){this.submitted = true}
+
+  get diagnostic(){return JSON.stringify(this.model)}
+
+
+
+ /*   this.rForm.get('gender').valueChanges.subscribe(
+      (validate) => {
+        if (validate == '1') {
+          this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+          this.titleAlert = "You need to specify at least 3 characters";
+        } else {
+          this.rForm.get('name').setValidators(Validators.required);
+        }
+        this.rForm.get('name').updateValueAndValidity();
+      }
+    )
+  }*/
+
+  /*addPost(post){
+    this.surname = post.surname;
+    this.surname = post.surname;
+    this.position = post.position;
+    this.id_number = post.id_number;
+    this.title = post.title;
+
+  }*/
 
   gotoCompany(): void {
     this.router.navigate(['/company']);
