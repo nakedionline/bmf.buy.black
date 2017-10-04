f61de00515213668180321ec7d7a0dddeae70da8
Guzzini <pmguzzini@gmail.com>
2017-10-04 19:16:01 +0200

    Capture_Company_Details_Validation

--- bmf-app/src/app/create-user/capture-company.component.ts
+++ bmf-app/src/app/create-user/capture-company.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Company} from '../services/company';
 
 @Component({
   selector: 'company',
@@ -8,8 +9,35 @@ import {Router} from '@angular/router';
 })
 
 export class CompanyRegistrationComponent implements OnInit{
+
+  name: string;
+  services: Array<string>;
+
+  model = new Company(
+    12,
+  '10001JC/200502',
+  'Mkhambeni Caterer',
+    'BCS123TX',
+    '42 Monte Carlo Str',
+    'DBN',
+    '3002',
+    '0112343333',
+    '0112340000',
+    'pm@home.com',
+    'Restaurant',
+    this.services,
+    '07h00-22h00'
+  );
+
+  submitted= false;
+
+
   constructor(private router: Router){}
   ngOnInit(){}
+
+  onSubmit(){this.submitted = true}
+
+  get diagnostic(){return JSON.stringify(this.model)}
 
   gotoUpload(): void {
     this.router.navigate(['/upload']);
